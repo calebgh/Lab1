802630e4180bc564ff2b27f544a887d58b8e674f
calebgh <calebc386@gmail.com>
2025-01-27 21:16:23 +0000

    finished lab

--- main.py
+++ main.py
@@ -13,5 +13,62 @@ with open('data.json') as f:
 @app.route('/')
 def hello_world():
     return 'Hello, World!'  # return 'Hello World' in response
+  
+@app.route('/students')
+def get_students():
+    return jsonify(data)# return student data in response
+
+@app.route('/hello/<string:a>/<string:b>')
+def hello_name(a,b):
+      return 'Hi ' + a + ' ' + b + '!'
+
+
+@app.route('/stats')
+def get_stats():
+  
+  chicken = 0
+  fish = 0
+  csm = 0
+  css = 0
+  itm = 0
+  its = 0
+  vege = 0
+  
+  for student in data:
+    if student['pref'] == "Chicken":
+       chicken += 1
+    if student['programme'] == "Computer Science (Major)":
+      csm += 1
+    if student['programme'] == "Computer Science (Special)":
+      css += 1
+    if student['pref'] == "Fish":
+      fish += 1
+    if student['programme'] == "Information Technology (Major)":
+      itm +=1
+    if student['programme'] == "Information Technology (Special)":
+      its +=1
+    if student['pref'] == "Vegetable":
+      vege += 1
+    
+  return jsonify({"Chicken": chicken,"Computer Science (Major)": csm , "Computer Science (Special)": css, "Fish": fish, "Information Technology (Major)": itm, "Information Technology (Special)": its, "Vegetable": vege })
+  
+
+@app.route('/add/<int:a>/<int:b>')
+def add_num(a,b):
+      return "Sum of " + str(a) + " and " + str(b) + " is " + str(a+b)
+
+
+@app.route('/subtract/<int:a>/<int:b>')
+def subtract_num(a,b):
+      return "Subtraction of " + str(a) + " and " + str(b) + " is " + str(a-b)
+
+@app.route('/multiply/<int:a>/<int:b>')
+def multiply_num(a,b):
+      return "Multiplication of " + str(a) + " and " + str(b) + " is " + str(a*b)
+
+@app.route('/divide/<int:a>/<int:b>')
+def divide_num(a,b):
+      return "Division of " + str(a) + " and " + str(b) + " is " + str(a/b)
+
 
 app.run(host='0.0.0.0', port=8080, debug=True)
